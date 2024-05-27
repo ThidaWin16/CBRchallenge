62f3b324343528ca4230f232680ce5c541dc56d4
ThidaWin <144346609+ThidaWin16@users.noreply.github.com>
2024-05-27 11:22:09 +0200

    Lukasinfo

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CBR vragen</title>
+</head>
+<body>
+
+<button type="button" class="btn btn-outline-primary">keuze1</button>
+<button type="button" class="btn btn-outline-secondary">keuze2</button>
+<button type="button" class="btn btn-outline-success">keuze3</button>
+<button type="button" class="btn btn-outline-danger">keuze4</button>
+
+</body>
+</html>
