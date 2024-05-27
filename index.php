a49bbeb572aa6a7e22d2c465ded6f4f911b8f598
ThidaWin <144346609+ThidaWin16@users.noreply.github.com>
2024-05-27 11:42:54 +0200

    class

--- index.php
+++ index.php
@@ -8,10 +8,12 @@
 </head>
 <body>
 
+<div class= "buttons">
 <button type="button" class="btn btn-outline-primary">keuze1</button>
 <button type="button" class="btn btn-outline-secondary">keuze2</button>
 <button type="button" class="btn btn-outline-success">keuze3</button>
 <button type="button" class="btn btn-outline-danger">keuze4</button>
+</div>
 
 </body>
 </html>
